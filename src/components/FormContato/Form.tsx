a3f1bf52a7d13bb1144d3429450ae4ab9bac9e5e
WdvOps <dvops@outlook.com>
2021-10-16 14:39:24 -0300

    i-portifolio pronto para produção

--- src/components/FormContato/Form.tsx
+++ src/components/FormContato/Form.tsx
@@ -1,18 +1,57 @@
-import React from 'react';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { sendContactMail } from '../../services/sendMail';
+import theme from '../../styles/theme';
 import { FormContainer, Input, TextArea } from './styles';
 
-function Form() {
+export default function Form() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
 
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [mensagem, setMensagem] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-    return (
-        <FormContainer>
-             <Input
+    if (!nome || !email || !mensagem) {
+      toast('Preencha todos os campos para enviar sua mensagem!', {
+        style: {
+          background: theme.error,
+          color: '#fff'
+        }
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      await sendContactMail(nome, email, mensagem);
+      setNome('');
+      setEmail('');
+      setMensagem('');
+
+      toast('Mensagem enviada com sucesso!', {
+        style: {
+          background: theme.secondary,
+          color: '#000'
+        }
+      });
+    } catch (error) {
+      toast('Ocorreu um erro ao tentar enviar sua mensagem. Tente novamente!', {
+        style: {
+          background: theme.error,
+          color: '#000'
+        }
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <FormContainer data-aos="fade-up" onSubmit={handleSubmit}>
+      <Input
         placeholder="Nome"
         value={nome}
         onChange={({ target }) => setNome(target.value)}
@@ -28,11 +67,9 @@ function Form() {
         value={mensagem}
         onChange={({ target }) => setMensagem(target.value)}
       />
-      <button type="submit" disabled={loading} >
+      <button type="submit" disabled={loading}>
         ENVIAR
       </button>
-        </FormContainer>
-    )
+    </FormContainer>
+  );
 }
-
-export default Form;
